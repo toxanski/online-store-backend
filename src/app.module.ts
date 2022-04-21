c2e0b72ae9fb230441d3fc593d96ca8ebd5fe700
detdom <tosha.smirnov.2002@gmail.com>
2022-04-22 00:18:05 +0300

    starting and setting up project;

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { TopPageModule } from './top-page/top-page.module';
+import { ReviewModule } from './review/review.module';
+
+@Module({
+  imports: [AuthModule, ProductModule, TopPageModule, ReviewModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
